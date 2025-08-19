ba9eba94978568279a81b1fa871739e92863f00f
vishalkannsgit <vishalkanns@outlook.com>
2025-08-19 10:47:46 +0530

    Add portfolio files

---
+++ config.js
@@ -0,0 +1,5 @@
+
+// config.js
+export const CONFIG = {
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbydGCMlP0jKve3wSrZL-_vH_vv8XhMrMy1w5KmHsS-jaOhzOjxE9dKbPgjSSdsslkTCxw/exec"
+};
